5e2dab8a8a913707e96da7fc5769528812e9e7b1
Vladislav Korecky <vladislav@korecky.org>
2022-11-20 13:55:58 +0100

    Přidáno view Rooms

--- PythonRemoteCZ9/urls.py
+++ PythonRemoteCZ9/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import base.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', base.views.hello),
+    path('', base.views.rooms),
 ]
---
+++ base/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from base.models import Room
+
+
+# Create your views here.
+def hello(request):
+    s = request.GET.get('s', '')
+    return HttpResponse(f'Ahoj {s}!!!')
+
+
+def rooms(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, template_name='base/rooms.html', context=context)
